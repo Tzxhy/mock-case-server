80a4c5da5f1457df339f0fc9617c0ade2a344c5a
tanzhixuan <tanzhixuan@bytedance.com>
2019-05-07 11:10:46 +0800

    v10

--- output/utils.d.ts
+++ output/utils.d.ts
@@ -6,7 +6,7 @@ import { Change, MockCase, Route } from "./MCS";
  */
 declare function findMatchIndexByUrlPath(path: string, caseItem: MockCase): number;
 declare function getRouteByUrlPath(path: string, caseItem: MockCase): Route | Change | undefined;
-declare const getCollectionWithMatchesAndRoute: (caseItem: MockCase) => (Change | Route)[];
+declare const getCollectionWithMatchesAndRoute: (caseItem: MockCase) => (Route | Change)[];
 /** 将数组中出现的字符串进行分组 */
 declare function groupName(arr: string[]): object;
 declare function getEnvKeyValue(key: string): string;
